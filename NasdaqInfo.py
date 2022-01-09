dec4649ef34ace39030cb57c3639f9023c1f434a
Thiago de Oliveira Souza <85824485+thiago-os@users.noreply.github.com>
2022-01-09 07:59:28 +0100

    added class to scrape non compliance list

--- NasdaqInfo.py
+++ NasdaqInfo.py
@@ -1,3 +1,9 @@
+import pandas as pd
+import requests
+import random
+import time
+from selenium import webdriver
+
 class TickerData:
 
     @staticmethod
@@ -21,9 +27,7 @@ class TickerData:
         'oldTicker': 'newTicker').
         user_agent_list: list of possible user agent headers to use randomly in the request call.
         """
-        import pandas as pd
-        import requests
-        import random
+        
         headers = [{'User-Agent': x} for x in user_agent_list]
         url = 'https://api.nasdaq.com/api/quote/list-type-extended/symbolchangehistory'
         header = random.choice(headers)
@@ -46,3 +50,65 @@ class TickerData:
             return sol
         else:
             return dict((x, y) for x, y in change_dict.values())
+
+
+class DelistWarning(object):
+    def __init__(self, df_raw=None, df=None):
+        self.df_raw = df_raw
+        self.df = df
+
+    @classmethod
+    def get_data(
+            cls,
+            chromedriver_path,
+            url='https://listingcenter.nasdaq.com/noncompliantcompanylist.aspx'
+    ):
+        """
+        Returns a DalistWarning object with two attributes: df and df_raw. The main output is df, a pandas
+        dataframe with columns symbol, deficiency (i.e., non compliance), market, and (notification) date.
+        df is not indexed by symbol (there can be repeated symbols).
+        df_raw is the same as df, but symbol_list replaces symbol (because some deficiencies
+        relate to many symbols).
+
+        :param chromedriver_path: Path for chromedriver.exe (https://chromedriver.chromium.org/).
+        :param url: nasdaq page to be scraped.
+        :return: DelistWarning object w/ df and df_raw pandas dataframes as attributes.
+        """
+        cols = ('symbol_list', 'deficiency', 'market', 'date')
+
+        df = pd.DataFrame(columns=cols)
+        driver = webdriver.Chrome(executable_path=chromedriver_path)
+
+        driver.get(url)
+        button = driver.find_element_by_class_name("rgExpand")
+        button.click()
+
+        # Wait for loading
+        while len(driver.find_elements_by_class_name("rgAltRow")) == 0:
+            time.sleep(1)
+
+        # finish loading(?)
+        time.sleep(2)
+
+        mytable = driver.find_element_by_class_name('rgMasterTable')
+
+        i = 0
+        for row in mytable.find_elements_by_class_name("rgAltRow"):
+            j = 0
+            i += 1
+            for cell in row.find_elements_by_css_selector('td'):
+                if cell.text.strip():
+                    if cols[j] == 'symbol_list':
+                        df.loc[i, cols[j]] = cell.text.strip().split('  ')
+                    else:
+                        df.loc[i, cols[j]] = cell.text.strip()
+                    j += 1
+        driver.close()
+
+        # Adjust df columns:
+        df2 = df.explode('symbol_list').reset_index(drop=True)
+        df2 = df2.rename(columns={'symbol_list': 'symbol'})
+        df2['date'] = pd.to_datetime(df2['date'])
+        df2 = df2.drop_duplicates()
+        res = cls(df_raw=df, df=df2)
+        return res
